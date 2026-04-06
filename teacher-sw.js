7ab6ac42b660759a2619bbce17e5ed061df807ff
rune1216-max <rune1216@gmail.com>
2026-04-06 16:33:58 +1000

    Add files via upload

---
+++ teacher-sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'teacher-tools-v1';
+const ASSETS = [
+  './teacher.html',
+  'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300;400;700;900&family=Noto+Serif+SC:wght@400;700&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
